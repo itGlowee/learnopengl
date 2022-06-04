b7949ac046494aed7163b03a385a32fc58eceb3f
Glowee <glowee@mailbox.org>
2022-06-05 00:20:33 +0300

    Magic and truetype added

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 #include <stdio.h>
 #define WINDOWWIDTH 800
 #define WINDOWHEIGHT 600
+
+#define WINDOWS 1
+#define LINUX 0
+
+#if WINDOWS
+#define FONTS "C:/Windows/Fonts/"
+#elif LINUX
+#define FONTS "/usr/share/fonts/truetype/"
+#endif
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -35,7 +47,30 @@ int main() {
     glViewport(0, 0, WINDOWWIDTH, WINDOWHEIGHT);
     glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
+    FT_Library ft;
+    FT_Error error;
+    if (FT_Init_FreeType(&ft))
+    {
+        printf("ERROR::FREETYPE: Could not init FreeType Library\n");
+        return -1;
+    }
+
+    FT_Face face;
+    if (error = FT_New_Face(ft, FONTS "arial.ttf", 0, &face))
+    {
+        printf("ERROR::FREETYPE: Failed to load font\n");
+        printf("%d\n", error);
+        return -1;
+    }
+
+
+
     while(!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
